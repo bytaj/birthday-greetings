0efacb68e78348aa9181a1ae6bc611794a41eade
Vera <vera.mamby@gmail.com>
2023-04-16 13:20:29 +0200

    Setup fully with docker bug fixed

--- tests/AcceptanceTest.php
+++ tests/AcceptanceTest.php
@@ -15,7 +15,7 @@ use Symfony\Component\Process\Process;
 
 final class AcceptanceTest extends TestCase
 {
-    private const SMTP_HOST = '127.0.0.1';
+    private const SMTP_HOST = 'mailhog';
     private const SMTP_PORT = 1025;
 
     private BirthdayService $service;
@@ -23,23 +23,13 @@ final class AcceptanceTest extends TestCase
     #[Before]
     protected function startMailhog(): void
     {
-        $whichDockerCompose = Process::fromShellCommandline('which docker-compose');
-        $whichDockerCompose->run();
-
-        if ('' === $whichDockerCompose->getOutput()) {
-            $this->markTestSkipped('To run this test you should have docker-compose installed.');
-        }
-
-        Process::fromShellCommandline('docker stop $(docker ps -a)')->run();
-        Process::fromShellCommandline('docker compose up -d')->run();
-
         $this->service = new BirthdayService();
     }
 
     #[After]
     protected function stopMailhog(): void
     {
-        (new Client())->delete('http://127.0.0.1:8025/api/v1/messages');
+        (new Client())->delete('http://mailhog:8025/api/v1/messages');
         Process::fromShellCommandline('docker compose stop')->run();
         Process::fromShellCommandline('docker compose rm -f')->run();
     }
@@ -79,6 +69,6 @@ final class AcceptanceTest extends TestCase
 
     private function messagesSent(): array
     {
-        return json_decode(file_get_contents('http://127.0.0.1:8025/api/v1/messages'), true);
+        return json_decode(file_get_contents('http://mailhog:8025/api/v1/messages'), true);
     }
 }
